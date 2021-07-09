968f437b7541a5c692e887be102e0b062dbd1952
Matt Koenig <mattkoenig@Matts-MacBook-Pro.local>
2021-07-09 13:07:44 -0700

    adding readme and setup.py

---
+++ curling_league/README.md
@@ -0,0 +1,3 @@
+## Python sports league manager! 
+
+This is a sports league manager that can be used with the provided UI or as a headless API to manage a sports league via email or some other interface. 
---
+++ curling_league/setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='Module6',
+      version='1.0',
+      description='Sports League Manager',
+      author='Matt Koenig',
+      author_email='matthewkoenig@acm.org',
+      url='https://github.com/mattwbdv/Sports-League-Manager',
+      packages=['distutils', 'distutils.command'],
+     )
